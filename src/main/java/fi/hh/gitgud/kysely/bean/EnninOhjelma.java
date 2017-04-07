f6f16f0112fd268b20d4624470f36d4742e1354f
Linnus Enni <a1303485@myy.haaga-helia.fi>
2017-04-07 15:47:49 +0300

    ennin ohjelmalisäys

---
+++ src/main/java/fi/hh/gitgud/kysely/bean/EnninOhjelma.java
@@ -0,0 +1,12 @@
+package fi.hh.gitgud.kysely.bean;
+
+public class EnninOhjelma {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("hauki on kala ");
+		}
+
+	}
+
+}
