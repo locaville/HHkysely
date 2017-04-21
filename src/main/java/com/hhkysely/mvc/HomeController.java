bceeadede24b87f26d595feeb13a0a9c55f3da98
admin <admin>
2017-04-21 11:49:43 +0300

    Lisätty metodeja controlleriin

--- src/main/java/com/hhkysely/mvc/HomeController.java
+++ src/main/java/com/hhkysely/mvc/HomeController.java
@@ -5,12 +5,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hhkysely.dao.kyselyDAO;
 
 /**
  * Handles requests for the application home page.
@@ -19,6 +23,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Inject
+	private kyselyDAO dao;
+	
+	public kyselyDAO getDAO() {
+		return dao;
+	}
+	
+	public void setDAO(kyselyDAO dao) {
+		this.dao = dao;
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
